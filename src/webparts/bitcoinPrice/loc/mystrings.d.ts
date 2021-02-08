22cbd7a24d6e5d2e87b32d55a2bcb1a8e2ca95c0
Rob Pearmain <rob@ipear.co.uk>
2021-02-08 10:24:17 +0000

    Initial Commit

---
+++ src/webparts/bitcoinPrice/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBitcoinPriceWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BitcoinPriceWebPartStrings' {
+  const strings: IBitcoinPriceWebPartStrings;
+  export = strings;
+}
